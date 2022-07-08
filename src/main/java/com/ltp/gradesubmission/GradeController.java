f3c53aad91ac4c554bbe234c9a8b353b669363d9
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-07-08 22:49:34 +0100

    create a list of grades

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,15 +1,22 @@
 package com.ltp.gradesubmission;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+    private List<Grade> studentGrades = Arrays.asList(
+            new Grade("Harry", "Potions", "C-"),
+            new Grade("Hermione", "Arithmancy", "A+"),
+            new Grade("Ron", "Charms", "D+"));
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
