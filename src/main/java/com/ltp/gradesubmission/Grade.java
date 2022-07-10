3c79f014a71002a2dc0a3f4f3156ba27065f64e6
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-07-10 17:22:25 +0100

    add logic to update grades

--- src/main/java/com/ltp/gradesubmission/Grade.java
+++ src/main/java/com/ltp/gradesubmission/Grade.java
@@ -1,17 +1,15 @@
 package com.ltp.gradesubmission;
 
+import java.util.UUID;
+
 public class Grade {
     private String name;
     private String subject;
     private String score;
-
-    public Grade(String name, String subject, String score) {
-        this.name = name;
-        this.subject = subject;
-        this.score = score;
-    }
+    private String id;
 
     public Grade() {
+        id = UUID.randomUUID().toString();
     }
 
     public String getName() {
@@ -36,5 +34,13 @@ public class Grade {
 
     public void setScore(String score) {
         this.score = score;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
@@ -19,14 +20,30 @@ public class GradeController {
     }
 
     @GetMapping("/")
-    public String gradeForm(Model model) {
-        model.addAttribute("grade", new Grade());
+    public String gradeForm(Model model, @RequestParam(required = false) String id) {
+        var gradeIndex = getGradeIndex(id);
+        var grade = gradeIndex == -1000 ? new Grade() : studentGrades.get(gradeIndex);
+        model.addAttribute("grade", grade);
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitForm(Grade grade) {
-        studentGrades.add(grade);
+        var gradeIndex = getGradeIndex(grade.getId());
+        if (gradeIndex == -1000) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(gradeIndex, grade);
+        }
         return "redirect:/grades";
+    }
+
+    private Integer getGradeIndex(String id) {
+        for (var i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1000;
     }
 }
